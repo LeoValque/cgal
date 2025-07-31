992141a10912a7a95547cb4f09778d17d3e83e4a
lvalque <leo.valque@gmail.com>
2025-07-31 16:38:09 +0200

    rename a variable which is reserved in bash

--- Polygon_mesh_processing/benchmark/Polygon_mesh_processing/Performance/run_performance.sh
+++ Polygon_mesh_processing/benchmark/Polygon_mesh_processing/Performance/run_performance.sh
@@ -20,10 +20,10 @@ TMP_LOG=$(mktemp)
 /usr/bin/time -f "TIME:%e\nMEM:%M" timeout "$TIMEOUT"s "$EXE_PATH/performance_snap_polygon_soup" "$INPUT_FILE" "$GRID_SIZE" "$ERASE_ALL_DUPLICATE" 2> "$TMP_LOG"
 
 # Parse time and memory
-SECONDS=$(grep "TIME" "$TMP_LOG" | cut -d':' -f2)
+RUNNING_TIME=$(grep "TIME" "$TMP_LOG" | cut -d':' -f2)
 MEMORY=$(grep "MEM" "$TMP_LOG" | cut -d':' -f2)
 
 rm -f "$TMP_LOG"
 
 # Output JSON
-echo "{\"seconds\": \"$SECONDS\", \"memory_peaks\": \"$MEMORY\"}"
+echo "{\"seconds\": \"$RUNNING_TIME\", \"memory_peaks\": \"$MEMORY\"}"
